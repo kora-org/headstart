6a6a891a4518b5435b38aece1c02b50379292c87
Leap of Azzam <leapofazzam@gmail.com>
2021-10-17 20:51:53 +0700

    dummy menu

--- stage1/uefi/stage1.c
+++ stage1/uefi/stage1.c
@@ -1,7 +1,17 @@
 #include <uefi.h>
+#include <stdbool.h>
 
-int main(int argc, char **argv)
-{
-    printf("Hello World!\n");
+int main(int argc, char **argv) {
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGREEN);
+    printf("\n  XeptoBoot ");
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY);
+    printf("0.1\n\n");
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE);
+    printf("> Test\n");
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY);
+    printf("  Test\n");
+    printf("  Test\n");
+    while (true)
+        asm("hlt");
     return 0;
 }
