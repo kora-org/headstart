6349960b75718d394279be39987ead0fd74bc6c0
Leap of Azzam <leapofazzam@gmail.com>
2021-11-16 19:26:31 +0700

    uefi graphics stuff
    Signed-off-by: Leap of Azzam <leapofazzam@gmail.com>

--- stage1/stage1.zig
+++ stage1/stage1.zig
@@ -1,12 +1,19 @@
 const console = @import("console");
+const graphics = @import("graphics");
 const io = @import("io");
 const builtin = @import("builtin");
 const uefi = @import("std").os.uefi;
 
-pub export fn stage1_entry(ST: *uefi.tables.SystemTable, BS: *uefi.tables.BootServices) void {
+pub var system_table: *uefi.tables.SystemTable = undefined;
+pub var boot_services: *uefi.tables.BootServices = undefined;
+
+pub export fn stage1_entry(_system_table: *uefi.tables.SystemTable, _boot_services: *uefi.tables.BootServices) void {
+    system_table = _system_table;
+    boot_services = _boot_services;
     io.outb(0xe9, 't');
+    graphics.initialize();
     console.initialize();
-    console.puts("Hello Bootloader World!");
+    console.printf("Hello {s} World!", .{ "Bootloader" });
     while (true)
         asm volatile("hlt");
 }
